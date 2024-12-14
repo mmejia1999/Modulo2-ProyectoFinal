5e67e18762e0e974f25ec94945ddbfb9a450a39e
Mich <mejia.cambara.michelle@gmail.com>
2024-12-14 03:09:19 -0600

    "Estructurando seccion contacto"

--- main.js
+++ main.js
@@ -7,4 +7,20 @@ function redirigir(url){
     window.location.href = url;
 }
 
+var acc = document.getElementsByClassName("accordion");
+      var i;
 
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          this.parentElement.classList.toggle("active");
+
+          var pannel = this.nextElementSibling;
+
+          if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+          } else {
+            pannel.style.display = "block";
+          }
+        });
+      }
